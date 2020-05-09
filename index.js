c5ead45ba7f3d1dc1ae36303a90b249bc5a3dbae
anthonygedeon <anthony.gedeonn@gmail.com>
2020-05-09 13:12:21 -0500

    can switch between links

--- index.js
+++ index.js
@@ -3,28 +3,17 @@ const url = require('url');
 const fs = require('fs');
 
 http.createServer((req, res) => {
-    const query = url.parse('http://localhost:8080', true);
-    let filename = `.${query.pathname}`;
-    
-    console.log(query.search)
-    
-    if (query.host === 'localhost:8080') {
-        filename = `./index.html`;
-    } else if (filename === './about') {
-        filename = './about.html';
-    } else if (filename === './contact-me') {
-        filename = './contact-me.html';
-    } else if (filename === './index') {
-        filename = './index.html';
-    } else {
-        filename = '404.html';
-    }
+    const query = url.parse(req.url, true);
+    const filename = query.pathname === '/' ? './index.html' : `.${query.pathname}.html`;
     
     fs.readFile(filename, (error, data) => {
 
         if (error) {
-            res.writeHead(404, {'Content-type': 'text/html'});
-            return res.end(data);
+            fs.readFile('404.html', (error, data) => {
+                res.writeHead(404, {'Content-type': 'text/html'});
+                res.write(data)
+                return res.end();
+            })
         }
 
         res.writeHead(200, {'Content-type': 'text/html'});
