ff1a7ec207efde3bcc24568896e66ae93d606624
anthonygedeon <anthony.gedeonn@gmail.com>
2020-05-10 12:49:35 -0500

    trying to use data from files instead of hardcoding in the html in res.end

--- index.js
+++ index.js
@@ -2,37 +2,27 @@ const http = require('http');
 const url = require('url');
 const fs = require('fs');
 
+const PORT = 8080;
+const MIME_TEXT_HTML = { 'Content-Type': 'text/html' };
+
 http.createServer((req, res) => {
 	const query = url.parse(req.url, true);
 	const filename = query.pathname === '/' ? './index.html' : `.${query.pathname}.html`;
 
-
 	fs.readFile(filename, (error, data) => {
 		if (error) {
-			res.writeHead(404, { 'Content-type': 'text/html' });
-			res.write(`<!DOCTYPE html>
-            <html lang="en">
-            
-            <head>
-                <meta charset="UTF-8">
-                <meta name="viewport" content="width=device-width, initial-scale=1.0">
-                <title>404</title>
-            </head>
-            
-            <body>
-                <nav>
-                    <a href="/"> Back to Home</a>
-                </nav>
-                <h1>404 - Sorry Not Found</h1>
-            
-            </body>
-
-            </html>`);
-			return res.end();
+			fs.readFile('./404.html', (error, data) => {
+                if (error) {
+                    res.writeHead(500, MIME_TEXT_HTML);
+                    res.end('Internal error!');
+                } else {
+                    res.writeHead(404, MIME_TEXT_HTML);
+                    res.end(data)
+                }
+            });
 		}
-
-		res.writeHead(200, { 'Content-type': 'text/html' });
+		res.writeHead(200, MIME_TEXT_HTML);
 		res.write(data);
 		return res.end();
 	});
-}).listen(8080);
+}).listen(PORT);
